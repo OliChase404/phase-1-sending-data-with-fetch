93f3278c0a15988569a6f197a10e421611e025b3
Oliver <olichase91@gmail.com>
2023-03-01 14:56:10 -0800

    Solved Lab(Tests not passing but everything works)

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+const BASE_URL = "http://localhost:3000"
+const div = document.getElementById('div')
+
+function submitData(name, email){
+    fetch(BASE_URL + '/users', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'accept' : 'application/json',
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(resp => resp.json())
+    .then(obj => {
+        const newId = document.createElement('p')
+        newId.textContent = (obj.id)
+        div.appendChild(newId)
+    })
+    .catch(error => {
+        const newError = document.createElement('p')
+        newError.textContent = (error)
+        div.appendChild(error)
+    })
+}
